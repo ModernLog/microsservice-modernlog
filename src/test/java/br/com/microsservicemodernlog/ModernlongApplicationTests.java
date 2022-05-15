dbbaf0fa524e170aac21ae7a23ce722937d1311f
Henrique Teixeira Conti <49920796+henriqueconti@users.noreply.github.com>
2022-05-15 15:51:27 -0300

    commit inicial

---
+++ src/test/java/br/com/microsservicemodernlog/ModernlongApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microsservicemodernlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernlongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
